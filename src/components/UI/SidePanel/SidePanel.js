42bc6d7793e822c5962a6df82d34a3eddfd70987
Sushant Mishra <70423224+sushantdeveloper@users.noreply.github.com>
2021-12-21 10:39:48 +0530

    path fixing in side panel

--- src/components/UI/SidePanel/SidePanel.js
+++ src/components/UI/SidePanel/SidePanel.js
@@ -5,38 +5,38 @@ import { useHistory } from "react-router";
 
 export const SidePanel = (props) => {
 
-    let navigate = useHistory();
+    let history = useHistory();
 
 
     let classs = [classes.SidePanel];
     if(props.show) classs.push(classes.Show);
+
+    const navigate = (path, type)=> {
+        if(type === 'push') {
+            history.push({ 
+                pathname:path
+            });
+        }
+        if(type === 'replace') {
+            history.replace({ 
+                pathname:path
+            });
+        }
+        props.toggleSidePanel(false);
+
+    }
     return (
         <>
             <Backdrop show={props.show} clicked={props.toggleSidePanel}/>
             <div className={classs.join(" ")}>
                 <ul>
-                    <li onClick={() => {
-                        navigate.replace({ 
-                            pathname:'/about'
-                        });
-                        props.toggleSidePanel(false);
-                    }}>
+                    <li onClick={() => navigate('/about', 'replace')}>
                         <div><h1>About</h1></div>
                     </li>
-                    <li onClick={() => {
-                        navigate.push({ 
-                            pathname:'/work'
-                        });
-                        props.toggleSidePanel(false);
-                    }}>
+                    <li onClick={() => navigate('/work', 'push')}>
                         <div><h1>Work</h1></div>
                     </li>
-                    <li onClick={() => {
-                        navigate.push({ 
-                            pathname:'/work'
-                        });
-                        props.toggleSidePanel(false);
-                    }}>
+                    <li onClick={() => navigate('/contact', 'push')}>
                         <div><h1>Contact</h1></div>
                     </li>
                 </ul>
--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import classes from './Contact.css';
 import Footer from '../../components/Footer/Footer';
+import {motion} from 'framer-motion'
 
 class Contact extends Component{
 
     render(){
         return(
             <div className={classes.Contact}>
-                <div className={classes.Content}>
-                    <a href='mailto:sushantkumar6700@gmail.com'><h1>Let's Connect</h1></a>
-                </div>
-                <Footer />
+                <motion.div
+                initial={{opacity:0, transform:'translateY(100%)'}}
+                animate={{opacity:1,transform:'translateY(0%)'}}
+                exit={{opacity:0}}
+                transition={{duration:0.55}}
+                >
+                    <div className={classes.Content}>
+                        <a href='mailto:sushantkumar6700@gmail.com'><h1>Let's Connect</h1></a>
+                    </div>
+                    <Footer />
+                </motion.div>
             </div>
         );
     }
